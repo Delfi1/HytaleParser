3fa937468a176387a4b420463f4e53a5802fe08d
Delfi <fezdxx@gmail.com>
2024-07-07 13:16:27 +0300

    Image downloader

--- hytale.py
+++ hytale.py
@@ -1,4 +1,6 @@
+import os
 from typing import Any, List
+import requests
 import json
 import glob
 import aiohttp
@@ -32,7 +34,7 @@ async def get_all_posts() -> List[Post]:
 
     return [Post(data) for data in result]
 
-async def download_video(id: str):
+async def download_clip(id: str):
     print(f"Downloading {id}...", end=' ')
     yt_opts = {'verbose': True,'outtmpl': 'clips/%(title)s.%(ext)s'}
 
@@ -48,7 +50,24 @@ async def download_video(id: str):
     file = glob.glob(f"{id}.*", root_dir="./clips")
     return file[0]
     
-async def get_all_videos(post: Post) -> list:
+async def download_image(url: str):
+    img = url[url.rfind('/')+1:]
+    print(f"Downloading {img}...", end=' ')
+    
+    file = glob.glob(img, root_dir="./images")
+    if file:
+        print("Already installed, skip")
+        return
+    
+    path = f"./images/{img}"
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    with open(path, 'wb') as handle:
+        data = requests.get(url, stream=True)
+        handle.write(data.content)
+
+    print("Success")
+
+async def get_all_clips(post: Post) -> list:
     url = post.url()
 
     request = urllib.request.Request(url, headers=headers)
@@ -63,25 +82,57 @@ async def get_all_videos(post: Post) -> list:
         try:
             if element['class'][0] == "video-container":
                 stream = element.find("stream")
-                
+
                 if stream:
                     result.append(stream["src"])
-        except Exception as e:
-            ...
+        except Exception as e: ...
     
     return result
 
-async def get_media_clips() -> list:
+async def get_all_images(post: Post) -> list:
+    url = post.url()
+
+    request = urllib.request.Request(url, headers=headers)
+    html = urllib.request.urlopen(request).read()
+    soup = BeautifulSoup(html, "html.parser")
+
+    body = soup.find("div", {"class": "post__body"})
+    imgs = body.find_all("img")
+
+    result = list()
+    for img in imgs:
+        try:
+            result.append(img['data-src'])
+        except Exception as e: ...
+
+    return result
+
+async def get_media() -> list:
     request = urllib.request.Request("https://hytale.com/media", headers=headers)
     html = urllib.request.urlopen(request).read()
     soup = BeautifulSoup(html, "html.parser")
     
     raw_media = soup.select_one("script:-soup-contains('clips')").string
-    media = json.loads(raw_media[raw_media.index("["):raw_media.rindex("]")+1])[0]['media']
+    return json.loads(raw_media[raw_media.index("["):raw_media.rindex("]")+1])[0]['media']
+
+# Returns list of clips uuids
+async def get_media_clips() -> list:
+    media = await get_media()
 
     clips = media['clips']
 
     result = list()
     for clip in clips:
         result.append(clip['src'])
+    return result
+
+# Rerurns list of links
+async def get_media_images() -> list:
+    media = await get_media()
+    images = media['screenshots'] + media['conceptArt']
+
+    result = list()
+    for image in images:
+        result.append(f"https://cdn.hytale.com/{image['src']}")
+
     return result
--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 import asyncio, hytale
 
-async def download_all():    
+async def download_all():
+    # get all posts
     posts = await hytale.get_all_posts()
+
+    # get clips from all posts
+    print("Downloading clips...")
     clips_ids = list()
     for post in posts:
-        print("Fetch video ids...")
-        print(post.title)
-        content = await hytale.get_all_videos(post)
+        print(f"Fetch clips ids in {post.title}...")
+        content = await hytale.get_all_clips(post)
         print(f"Post clips: {content}")
         clips_ids.extend(id for id in content if id not in clips_ids)
     # get clips from /media
     clips_ids.extend(id for id in await hytale.get_media_clips() if id not in clips_ids)
 
-    print(f"Clipss uuids: {clips_ids}")
     for id in clips_ids:
-        await hytale.download_video(id)
+        await hytale.download_clip(id)
+
+    # get images from all posts
+    print("Dowloading images...")
+    images_url = list()
+    for post in posts:
+        print(f"Fetch images in {post.title}...")
+        content = await hytale.get_all_images(post)
+        print(f"Post images: {content}")
+        images_url.extend(url for url in content if url not in images_url)
+    # get images from /media
+    images_url.extend(url for url in await hytale.get_media_images() if url not in images_url)
+    
+    for url in images_url:
+        await hytale.download_image(url)
 
 def main():
     asyncio.run(download_all())
